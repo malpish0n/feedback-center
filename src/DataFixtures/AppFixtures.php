1ee23b1cff20203cc9ca676bea04bd50b0f8b5cd
Arek <akasztelan617@gmail.com>
2025-04-16 22:11:40 +0200

    removed useless migrations

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $article = new Article();
+
+        $article->setTitle('Tytul');
+        
+        $manager->persist($article);
 
         $manager->flush();
     }
