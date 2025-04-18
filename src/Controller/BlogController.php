6be6cb46dbf5766b9e2e18cd8a48500cc5a3f5ff
Arek <akasztelan617@gmail.com>
2025-04-18 19:24:53 +0200

    Updated BlogController to show data from database

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,11 +1,21 @@
 <?php
 declare(strict_types=1);
 namespace App\Controller;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 class BlogController extends AbstractController
 {
+
+    public function __construct(
+        private ArticleRepository $articleRepository
+    )
+    {  
+    }
+
+
     #[Route('/main-page',name:'main_page')]
     public function mainPage(): Response
     {
@@ -13,3 +23,4 @@ class BlogController extends AbstractController
         return new Response(print_r($articles,true));
     }
 }
+
