db31a507772491dda236ec4b32ada3aff83adc4a
Arek <akasztelan617@gmail.com>
2025-04-19 11:28:52 +0200

    Updated ArticleProvider

--- src/Service/ArticleProvider.php
+++ src/Service/ArticleProvider.php
@@ -6,5 +6,16 @@ namespace App\Service;
 
 class ArticleProvider
 {
-    
+    public function transformDataForTwig(array $articles): array
+    {
+        $transformedData = [];
+        foreach ($articles as $article) {
+            $transformedData['articles'][] = [
+                'title' => $article->getTitle(),
+                'link' => 'article/' . $article->getId(),
+            ];
+        }
+        
+        return $transformedData;
+    }
 }
