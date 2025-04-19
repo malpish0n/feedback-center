6317f54019786b44b66c27e09a3f78d16f51fff2
Arek <akasztelan617@gmail.com>
2025-04-19 11:49:48 +0200

    Updated BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,23 +5,29 @@ use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\ArticleProvider;
+
 
 class BlogController extends AbstractController
 {
 
     public function __construct(
-        private ArticleRepository $articleRepository
+        private ArticleRepository $articleRepository,
+        private ArticleProvider $articleProvider
     )
     {  
     }
 
 
-    #[Route('/main-page',name:'main_page')]
-    public function mainPage(): Response
+    #[Route('/articles',name:'blog-articles')]
+    public function showArticles(): Response
     {
         $articles = $this->articleRepository->findAll();
-        dump($articles);
-        return new Response(print_r($articles,true));
+        $parameters = [];
+        if ($articles) {
+            $parameters = $this->articleProvider->transformDataForTwig($articles);
+        }
+        return $this->render('articles/articles.html.twig', $parameters);
     }
 }
 
