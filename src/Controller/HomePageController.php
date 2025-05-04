17681e7a800c71f73938ca61ddfd797a96cab80b
malpish0n <akasztelan617@gmail.com>
2025-05-04 10:32:19 +0200

    add form editing and record deletion for homepage content

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,8 +6,14 @@ use App\Repository\HomePageContentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\HomePageContent;
+use App\Form\HomePageContentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 
-final class HomePageController extends AbstractController
+#[AsController]
+class HomePageController extends AbstractController
 {
     #[Route('/home/page', name: 'app_home_page')]
     public function index(HomePageContentRepository $repository): Response
@@ -21,17 +27,49 @@ final class HomePageController extends AbstractController
     }
 
     #[Route('/home/page/{id}/edit', name: 'home_page_edit')]
-    public function edit(int $id): Response
-    {
-    // Tutaj będzie formularz edycji
-    return new Response("Edytuj rekord o ID: $id");
+    public function edit(
+        int $id,
+        HomePageContentRepository $repository,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        $item = $repository->find($id);
+    
+        if (!$item) {
+            throw $this->createNotFoundException("Nie znaleziono rekordu o ID $id");
+        }
+    
+        $form = $this->createForm(HomePageContentType::class, $item);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+    
+            return $this->redirectToRoute('app_home_page');
+        }
+    
+        return $this->render('home_page/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
+    
 
     #[Route('/home/page/{id}/delete', name: 'home_page_delete')]
-    public function delete(int $id): Response
-    {
-    // Tutaj będzie usuwanie rekordu
-    return new Response("Usuń rekord o ID: $id");
+    public function delete(
+        int $id,
+        HomePageContentRepository $repository,
+        EntityManagerInterface $em
+    ): Response {
+        $item = $repository->find($id);
+    
+        if (!$item) {
+            throw $this->createNotFoundException("Nie znaleziono rekordu o ID $id");
+        }
+    
+        $em->remove($item);
+        $em->flush();
+    
+        return $this->redirectToRoute('app_home_page');
     }
 
 }
