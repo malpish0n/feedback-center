b9ed3fb316263427a25ea2b406d3fa2fe5a86a25
malpish0n <akasztelan617@gmail.com>
2025-06-24 19:38:36 +0200

    Added post viewing on homepage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,16 +6,19 @@ use App\Repository\HomePageContentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PostRepository;
+
 
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(HomePageContentRepository $repository): Response
-    {
-        $items = $repository->findAll();
+public function index(PostRepository $postRepo): Response
+{
+    $posts = $postRepo->findBy([], ['id' => 'DESC']);
 
-        return $this->render('home_page/index.html.twig', [
-            'items' => $items,
-        ]);
-    }
+    return $this->render('home_page/index.html.twig', [
+        'posts' => $posts,
+    ]);
+}
+
 }
